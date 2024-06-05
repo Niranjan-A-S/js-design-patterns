a6d322a3240a76711e70104a2a2cfc1403e96dd4
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-06-05 12:56:31 +0530

    chore: complete the implementation of observer pattern

--- vannila/design/observer/react-app/src/App.jsx
+++ vannila/design/observer/react-app/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ToastContainer, toast } from "react-toastify";
+import { Observable } from "../observer";
+
+const observable = new Observable()
+observable.subscribe(logger);
+observable.subscribe(toastify);
+
+function logger(data) {
+  console.log(`${Date.now()}: ${data}`)
+}
+
+function toastify(data) {
+  toast(data);
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    observable.notify("hello world");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleClick}>Click</button>
+      <ToastContainer autoClose closeButton={false} />   
     </>
   )
 }
